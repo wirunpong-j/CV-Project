94640a346af5de77380fe7862901eb1cbc98f16a
Wirunpong Jaingamlertwong <wirunpong.j@outlook.com>
2017-11-16 09:47:25 +0700

    initial project

---
+++ cv.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+
+def main():
+	image = cv2.imread("test.jpg")
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	f, ax = plt.subplots()
+	f.canvas.set_window_title("image naja")
+	ax.imshow(image, interpolation = 'none')
+	plt.show()
+
+main()
