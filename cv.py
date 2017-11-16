c0c57cb5b86c55d514a76edae77d77c5099177d7
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-16 11:15:08 +0700

    add scan function

--- cv.py
+++ cv.py
@@ -1,20 +1,97 @@
-import cv2
 import matplotlib
 matplotlib.use('TkAgg')
 
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import the necessary packages
+from transform import four_point_transform
+import imutils
+from skimage.filters import threshold_adaptive
 import numpy as np
+import argparse
+import cv2
+ 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+	help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
 
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
 
+#increase Contrast
+lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+cv2.imshow("lab",lab)
 
-def main():
-	image = cv2.imread("test.jpg")
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#-----Splitting the LAB image to different channels-------------------------
+l, a, b = cv2.split(lab)
 
-	f, ax = plt.subplots()
-	f.canvas.set_window_title("image naja")
-	ax.imshow(image, interpolation = 'none')
-	plt.show()
+#-----Applying CLAHE to L-channel-------------------------------------------
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+cl = clahe.apply(l)
+cv2.imshow('CLAHE output', cl)
 
-main()
+#-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+limg = cv2.merge((cl,a,b))
+
+#-----Converting image from LAB Color model to RGB model--------------------
+image = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+ 
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+edged = cv2.Canny(gray, 75, 200)
+ 
+# show the original image and the edge detected image
+print "STEP 1: Edge Detection"
+cv2.imshow("Image", image)
+cv2.imshow("Grey", gray)
+
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+ 
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+ 
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+ 
+# show the contour (outline) of the piece of paper
+print "STEP 2: Find contours of paper"
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# apply the four point transform to obtain a top-down
+# view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+ 
+# convert the warped image to grayscale, then threshold it
+# to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+warped = threshold_adaptive(warped, 251, offset = 10)
+warped = warped.astype("uint8") * 255
+ 
+# show the original and scanned images
+print "STEP 3: Apply perspective transform"
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
